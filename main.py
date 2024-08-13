076bb5e30e46f9c4e4df7098368b73d808f8a25b
hwarange <asdf78964@naver.com>
2024-08-13 16:07:24 +0900

    main함수

---
+++ main.py
@@ -0,0 +1,24 @@
+def main():
+    """Main function to run the banking system."""
+    while True:
+        show_menu()
+        choice = menu_input()
+        print()
+
+        if choice == 1:
+            make_account()
+        elif choice == 2:
+            deposit_money()
+        elif choice == 3:
+            withdraw_money()
+        elif choice == 4:
+            show_all_account_information()
+        elif choice == 5:
+            print("프로그램을 종료합니다.")
+            break
+        else:
+            print("Illegal selection..")
+            print()
+
+if __name__ == "__main__":
+    main()
